038f0d1615759529aeb44653573e1d54816e3f0c
Test-Aviato <testing@aviato.consulting>
2025-08-07 07:01:18 +0530

    Initial commit

--- output/fast/stages/0-bootstrap/variables.tf
+++ output/fast/stages/0-bootstrap/variables.tf
@@ -1,42 +1,55 @@
-/**
- * Copyright 2025 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-variable "resource_names" {
-  description = "Resource names overrides for specific resources. Prefix is always set via code."
-  type = object({
-    bq-billing           = optional(string, "billing_export")
-    bq-logs              = optional(string, "logs")
-    gcs-bootstrap        = optional(string, "prod-iac-core-bootstrap-0")
-    gcs-logs             = optional(string, "prod-audit-logs-0")
-    gcs-outputs          = optional(string, "prod-iac-core-outputs-0")
-    gcs-resman           = optional(string, "prod-iac-core-resman-0")
-    gcs-vpcsc            = optional(string, "prod-iac-core-vpcsc-0")
-    project-automation   = optional(string, "prod-iac-core-0")
-    project-billing      = optional(string, "prod-billing-exp-0")
-    project-logs         = optional(string, "prod-audit-logs-0")
-    pubsub-logs_template = optional(string, "$${key}")
-    sa-bootstrap         = optional(string, "prod-bootstrap-0")
-    sa-bootstrap_ro      = optional(string, "prod-bootstrap-0r")
-    sa-cicd_template     = optional(string, "prod-$${key}-1")
-    sa-cicd_template_ro  = optional(string, "prod-$${key}-1r")
-    sa-resman            = optional(string, "prod-resman-0")
-    sa-resman_ro         = optional(string, "prod-resman-0r")
-    sa-vpcsc             = optional(string, "prod-vpcsc-0")
-    sa-vpcsc_ro          = optional(string, "prod-vpcsc-0r")
-  })
+variable "log_sinks" {
+  description = "Org-level log sinks, in name => {type, filter} format."
+  type = map(object({
+    filter     = string
+    type       = string
+    disabled   = optional(bool, false)
+    exclusions = optional(map(string), {})
+  }))
   nullable = false
-  default  = {}
+  default = {
+    audit-logs = {
+      # activity logs include Google Workspace / Cloud Identity logs
+      # exclude them via additional filter stanza if needed
+      filter = <<-FILTER
+        log_id("cloudaudit.googleapis.com/activity") OR
+        log_id("cloudaudit.googleapis.com/system_event") OR
+        log_id("cloudaudit.googleapis.com/policy") OR
+        log_id("cloudaudit.googleapis.com/access_transparency")
+      FILTER
+      type   = "logging"
+      # exclusions = {
+      #   gke-audit = "protoPayload.serviceName=\"k8s.io\""
+      # }
+    }
+    iam = {
+      filter = <<-FILTER
+        protoPayload.serviceName="iamcredentials.googleapis.com" OR
+        protoPayload.serviceName="iam.googleapis.com" OR
+        protoPayload.serviceName="sts.googleapis.com"
+      FILTER
+      type   = "logging"
+    }
+    vpc-sc = {
+      filter = <<-FILTER
+        protoPayload.metadata.@type="type.googleapis.com/google.cloud.audit.VpcServiceControlAuditMetadata"
+      FILTER
+      type   = "logging"
+    }
+    workspace-audit-logs = {
+      filter = <<-FILTER
+        protoPayload.serviceName="admin.googleapis.com" OR
+        protoPayload.serviceName="cloudidentity.googleapis.com" OR
+        protoPayload.serviceName="login.googleapis.com"
+      FILTER
+      type   = "logging"
+    }
+  }
+  validation {
+    condition = alltrue([
+      for k, v in var.log_sinks :
+      contains(["bigquery", "logging", "project", "pubsub", "storage"], v.type)
+    ])
+    error_message = "Type must be one of 'bigquery', 'logging', 'project', 'pubsub', 'storage'."
+  }
 }
